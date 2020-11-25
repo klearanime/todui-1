0f3cbeeaddb6a15a1e57a626a55a5fc43b10d548
Crystal K Young <maybiz@hotmail.com>
2020-11-25 15:41:21 -0500

    add

--- main.js
+++ main.js
@@ -16,4 +16,24 @@ Your options are:
 5. Toggle a todo's priority.
 6. Quit.
 
-`
+`;
+
+// console.log(menu)
+
+const handleMenu = function(choice) {
+  interface.close()
+  if (choice === '6') {
+    interface.close()
+  } else if (choice === '1' || choice === '2' || choice === '3' || choice === '4' || choice === '5') {
+    console.log('Your selection is: ' + choice)
+    interface.question(menu, handleMenu)
+  } else {
+        console.log('You got it wrong AGAIN? Get out of here.')
+    }
+    interface.close()
+}
+
+console.clear()
+interface.question(menu, handleMenu)
+
+
